101a3527ec2ded0342a7d727d46e88c9949479ea
Denis Borisov <mai.borisov.denis@gmail.com>
2023-07-01 13:59:15 +0300

    init repository

---
+++ src/main/java/com/example/personjpqlservice/PersonJpqlServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.personjpqlservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonJpqlServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonJpqlServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/personjpqlservice/PersonJpqlServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.personjpqlservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonJpqlServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
